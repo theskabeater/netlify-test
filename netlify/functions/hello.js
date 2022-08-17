60aef1769c23398c67a70df7f4e1530247030b40
Eric Moncada <theskabeater@Erics-MacBook-Air.local>
2022-08-17 12:25:01 -0600

    test(hello): node-fetch, default node version

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,10 @@
+import fetch from "node-fetch";
+
 exports.handler = async function (event, context) {
+  const hasFetch = !!fetch;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: process.version }),
+    body: JSON.stringify({ message: hasFetch }),
   };
 };
