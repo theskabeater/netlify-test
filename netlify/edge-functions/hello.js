492a9a301f67dd9118880ea47e1ccbcfe5656eeb
Eric Moncada <theskabeater@Erics-MacBook-Air.local>
2022-08-17 13:23:37 -0600

    test(hello): default export, edge

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,9 +1,5 @@
-exports.handler = async function (event, context) {
+export default async () => {
   const response = await fetch("https://api.github.com/users/github");
   const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
-};
+  return new Response(JSON.stringify(data));
+}
